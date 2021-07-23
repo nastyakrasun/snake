2d9cbe3b8a00bb00cb2387149144927760365ae4
Nastya Krasun <nastyakrasun@gmail.com>
2021-07-23 15:33:18 +0300

    draw horisontal lines

--- Program.cs
+++ Program.cs
@@ -25,54 +25,8 @@ namespace Snake
 			Point p5 = new Point(9, 11, '%');
 			/*p5.Draw();*/
 
-			Console.WriteLine();
-			Console.WriteLine();
-
-			List<int> numList = new List<int>();
-			numList.Add(100);
-			numList.Add(200);
-			numList.Add(300);
-
-			int x = numList[0];
-			int y = numList[1];
-			int z = numList[2];
-
-			foreach(int i in numList)
-            {
-				Console.WriteLine(i);
-            }
-
-			numList.RemoveAt(0);
-
-			Console.WriteLine();
-
-			List<Point> pList = new List<Point>();
-			pList.Add(p1);
-			pList.Add(p2);
-			pList.Add(p3);
-			pList.Add(p4);
-			pList.Add(p5);
-
-			foreach (Point i in pList)
-			{
-				i.Draw();
-			}
-			
-			Console.WriteLine();
-
-			List<char> symList = new List<char>();
-			symList.Add('&');
-			symList.Add('*');
-			symList.Add('#');
-
-			char a = symList[0];
-			char b = symList[1];
-			char c = symList[2];
-
-			foreach (char i in symList)
-			{
-				Console.WriteLine(i);
-			}
+			HorisontalLine line = new HorisontalLine();
+			line.Draw();
 
 			Console.ReadLine();
 		}
